0c8f3a4fa13657dfeedd50f5f5486fe23d2c8719
RealBlueSwan <nobrunner@hotmail.com>
2024-10-13 14:57:50 +0200

    list-comprehensions a4 HS24

--- access/a4/HS24/list-comprehensions/task/script.py
+++ access/a4/HS24/list-comprehensions/task/script.py
@@ -5,21 +5,29 @@ def square_numbers(numbers):
     return [ number ** 2 for number in numbers ]
 
 def even_numbers(numbers):
-    pass
+    return [ number for number in numbers if number % 2 == 0 ]
 
 def all_even(numbers):
-    pass
+    return [ round(number / 2) * 2 if isinstance(number, float) else (number if number % 2 == 0 else number + 1) for number in numbers ]
 
 def only_integers(numbers):
-    pass
+    return [ number for number in numbers if type(number) == int]
 
 def only_positive(numbers):
-    pass
+    return [ abs(number) for number in numbers ]
 
 def from_1_to_1000_containing_a(a):
-    pass
+    return [ number for number in range(0, 1001) if str(a) in str(number) ]
 
 def multiple_of_a_and_greater_than_b(numbers, a, b):
-    pass
+    return [ number for number in numbers if number > b and number % a == 0]
 
+
+# print(square_numbers([1, 2, 3]))
+# print(even_numbers([1, 2, 3]))
+# print(all_even([1, 2, 3]))
+# print(only_integers([1.1, 2]))
+# print(only_positive([1, -2, -3]))
+# print(from_1_to_1000_containing_a(0))
+# print(multiple_of_a_and_greater_than_b([1, 4, 6], 2, 3))
 
