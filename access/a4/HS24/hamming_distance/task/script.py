f63ea545f14a5aaea022ac51428b05c2810e7f98
RealBlueSwan <nobrunner@hotmail.com>
2024-10-14 10:33:17 +0200

    hamming distance inworking(BUGdesc. at line: 30)

--- access/a4/HS24/hamming_distance/task/script.py
+++ access/a4/HS24/hamming_distance/task/script.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
-
-# Complete the following to implement the described hamming distance function.
-# This signature is required for the automated grading to work.
-# Do not rename the function or change its list of parameters!
-
 def hamming_dist(signal_1, signal_2):
-    pass
+    result = []
 
-# The following lines print your function's output for an exemplary input to the console.
-# Note that this does not include any of the mentioned edge cases for defective sensors or signals of different lenghts.
-# Try to write your own tests for this.
+    # Check if the lengths of times, data, and signal_2 are equal
+    if len(signal_1["times"]) != len(signal_1["data"]) or len(signal_1["data"]) != len(signal_2):
+        return "Empty signal on at least one of the sensors"
+    
+    # Iterate through the data
+    for i in range(len(signal_1["data"])):
+        counter = 0
+        if len(signal_1["data"][i]) != len(signal_2[i]):
+            return "Sensor defect detected"
+        # Calculate Hamming distance
+        for k in range(len(signal_2[i])):
+            if signal_1["data"][i][k] != signal_2[i][k]:
+                counter += 1
+        if counter != 0:
+            result.append((signal_1["data"][i], signal_2[i], counter))
+    
+    return result
 
-signal_sensor_1 = {"times": [0, 1, 2, 3, 4, 5],
-                   "data": ["00101110", "11001011", "11110000", "01000011", "11001101", "00011011"]}
-signal_sensor_2 = ("00101110", "11001001", "11110011", "01111011", "11001101", "00011011")
+# Example signals
+signal_sensor_1 = {"times": [0, 1, 2], "data": ["00110101", "01010101", "10001110"]}
+signal_sensor_2 = ("00110101", "01010101", "10001110")
 print(hamming_dist(signal_sensor_1, signal_sensor_2))
+
+# BUGReport 
+# !!!Return value was not a string for inputs where an empty-signal warning was expected!!!
