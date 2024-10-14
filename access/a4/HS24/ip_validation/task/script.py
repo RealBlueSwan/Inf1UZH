bc028a14c3c6ab6ca8966990cee781d2ac10cfc7
RealBlueSwan <nobrunner@hotmail.com>
2024-10-14 13:30:10 +0200

    added ip_validation for IPv4 at HS24 a4

--- access/a4/HS24/ip_validation/task/script.py
+++ access/a4/HS24/ip_validation/task/script.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 def is_valid_IPv4_octet(octet):
+
+    return str(octet).isdigit() and 0 <= int(octet) <= 255
     """Returns True if octet represents a valid IPv4 octet, False otherwise"""
-    pass
 
 def is_valid_IPv4(ip):
-    """Returns True if ip represents a valid IPv4 address, False otherwise"""
-    pass
+
+    # Check if ip consists of 4 octets 
+    if not len(ip.split('.')) == 4:
+        return False
+    
+    for i in ip.split('.'):
+        if not is_valid_IPv4_octet(str(i)):
+            return False
+        
+    return True
 
 def is_valid_IPv6_hextet(hextet):
     """Returns True if hextet represents a valid IPv6 hextet, False otherwise"""
@@ -22,3 +31,4 @@ def is_valid_IP(ip):
 
 # You should look at task/test.py and extend the test suite we provided!
 
+print(is_valid_IPv4("127.0.0.1"))
