330e2c7aabb87b5c74300e6d37c65edb89051fbe
RealBlueSwan <nobrunner@hotmail.com>
2024-10-14 15:28:19 +0200

    completed is_prime HS24 a4

--- access/a4/HS24/is_prime/task/script.py
+++ access/a4/HS24/is_prime/task/script.py
@@ -2,13 +2,27 @@
 
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
+#!/usr/bin/env python3
+
 def is_prime(n):
-    # implement this function
-    pass
+
+    if n <= 1:
+        return "1 is the multiplicative identity" if n == 1 else "Number is negative"
+    
+    if n <= 3:
+        return f"{n} is prime" if n > 1 else "1 is the multiplicative identity"
+    
+    if n % 2 == 0:
+        return f"{n} is not a prime number (2 * {n // 2} = {n})"
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return f"{n} is not a prime number ({i} * {n // i} = {n})"
+    
+    return f"{n} is prime"
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(is_prime(12))
-
+print(is_prime(1003))
