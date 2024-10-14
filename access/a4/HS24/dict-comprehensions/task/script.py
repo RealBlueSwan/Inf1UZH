21470755e4d9c3bd38ace38162f32e37081e96e3
RealBlueSwan <nobrunner@hotmail.com>
2024-10-14 12:57:00 +0200

    fixed an error at dict-comprehensions HS24 a4 where area dict would return not wrong values, but formated values that resulted in not all points

--- access/a4/HS24/dict-comprehensions/task/script.py
+++ access/a4/HS24/dict-comprehensions/task/script.py
@@ -8,31 +8,13 @@ def even_odd_dict(numbers: [int]):
     return {x: "even" if x % 2 == 0 else "odd" for x in numbers}
 
 def area_dict(numbers: [int]):
-    return {x: {"square": x*x, "circle": '{:.4f}'.format((x**2)* pi)} for x in numbers if x >= 0}
-"""
-# Expected return value:
-{
-    1: {
-        'square': 1,
-        'circle': 3.1415
-    },
-    2: {
-        'square': 4,
-        'circle': 12.566
-    },
-    3: {
-        'square': 9,
-        'circle': 28.2735
-    },
-}
-!!!The area_dict function does not return a dictionary with the right values. Check your output format.!!!
-"""
-
+    return { x: {"square": x**2, "circle": pi * x**2} for x in numbers if x >= 0 }
+    
 def smaller_larger_dict(numbers: [int]):
     return {x: {y: "larger" if y > x  else "smaller" for y in numbers if x != y}for x in numbers}
 
 
 # print(square_root_dict(number_list))
 # print(even_odd_dict(number_list))
-print(area_dict(number_list))
+# print(area_dict(number_list))
 # print(smaller_larger_dict(number_list))
