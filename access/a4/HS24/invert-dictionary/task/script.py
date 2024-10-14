ffb439b196728b2f4d3923fbeea9cfc0baec4cfb
RealBlueSwan <nobrunner@hotmail.com>
2024-10-14 12:48:41 +0200

    invert-dictionary HS24 a4 complete

--- access/a4/HS24/invert-dictionary/task/script.py
+++ access/a4/HS24/invert-dictionary/task/script.py
@@ -3,12 +3,19 @@
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def invert(d):
-    # implement this function
-    pass
-
+    result = {}
+    for k, v in d.items():
+        if v not in result:
+            result[v] = []
+        result[v].append(k)
+    # Sort the lists of keys for each value
+    for v in result:
+        result[v].sort()
+    return result
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(invert({"a":1, "b":1, "c":3}))
+print(invert({"a":1, "b":1, "c":3})) # should return {1: ["a", "b"], 3: ["c"]}
+print(invert({3: 2, 1: 2})) # should return {2: [1, 3]}
