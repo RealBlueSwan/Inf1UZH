19a0d98b4f3864190f70a7035e7a1c2a1e440248
RealBlueSwan <nobrunner@hotmail.com>
2024-10-20 16:52:57 +0200

    completed juliets-numbers HS24 a5

--- access/a5/HS24/juliets-number/task/script.py
+++ access/a5/HS24/juliets-number/task/script.py
@@ -9,10 +9,26 @@ wa_nrs = ["0781111119", "0792653913", "0797763139", "0792793193", "0781139022", 
 # This signature is required for the automated grading to work. 
 # Do not rename the function or change its list of parameters.
 def get_possible_nrs(n):
+
     # This function accepts a string n for juliets number where one digit is missing.
+    if len(n) != 9:
+        raise Exception(f"The provided Number {n} doesnt have 9 digits but {len(n)}!")
+    
     # and should return a list of all whatsapp numbers that might belong to juliet 
     possible_nrs_for_juliet = []
 
+    # I need to add a number everywhere and check if the number is in wa_nrs
+    for i in range(7):
+        for k in range(0, 10):
+            if (n[:i+2] + str(k) + n[i+2:]) in wa_nrs:
+                if (n[:i+2] + str(k) + n[i+2:]) not in possible_nrs_for_juliet:
+                    possible_nrs_for_juliet.append(n[:i+2] + str(k) + n[i+2:])
+    for k in range(0, 10):
+        if (n + str(k)) in wa_nrs:
+            if (n + str(k)) not in possible_nrs_for_juliet:
+                possible_nrs_for_juliet.append((n + str(k)))
+
+    return possible_nrs_for_juliet
     # Don't forget to return your result
 
 # For this particular number, the function should find the
