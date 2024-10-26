5b5cf61124740c68c02b997ab0ba62c90dd8b671
RealBlueSwan <nobrunner@hotmail.com>
2024-10-26 14:24:04 +0200

    completed sieve-of-eratosthenes HS24 a5

--- access/a5/HS24/sieve-of-eratosthenes/task/script.py
+++ access/a5/HS24/sieve-of-eratosthenes/task/script.py
@@ -5,16 +5,40 @@ import math
 
 # perform the Sieve of Eratosthenes algorithm and return all primes <= n
 def sieve_of_eratosthenes(n):
-    # You need to change the functionality of this function to
-    # create a (sorted) list of all primes <= n which will then be returned.
-    # Use the Sieve of Eratosthenes algorithm from the description.
-    # You may change the following initialization of the list to be returned.
     primes_up_to_n = []
+
+    if n < 2:
+        return primes_up_to_n
+    
+    prime = [True for i in range(n+1)]
+
+    prime[0] = False
+    prime[1] = False
+
+    p = 2
+
+    while (p*p <= n):
+        if prime[p] == True:
+            for i in range(p*p, n+1, p):
+                prime[i] = False
+        p += 1
+    
+    
+    for idx in range(n+1):                  # Now we convert all the True's into numbers 
+        if prime[idx] == True:
+            primes_up_to_n.append(idx)
+    
+    return primes_up_to_n
+
+    """
+    - Beware of modifying lists that are you are currently looping over. This can lead to skipped elements, for instance non-primes that remain in the list your function returns.
+    - Your implementation should be close to the Sieve of Eratosthenes algorithm, but you may use contains(), remove() etc. on lists which might slow your implementation down.
+    - Use math.sqrt() and math.ceil() to find the last integer you need to *sieve*.
+    """
 
     # You don't need to change the following line.
     # It simply returns the list created above.
-    return primes_up_to_n
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
-print(sieve_of_eratosthenes(1000))
+print(sieve_of_eratosthenes(100))
