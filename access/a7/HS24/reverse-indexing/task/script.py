68480b18be81566df1137f11504c8b9f23e0b81f
RealBlueSwan <nobrunner@hotmail.com>
2024-11-09 14:24:24 +0100

    cleanup

--- access/a7/HS24/reverse-indexing/task/script.py
+++ access/a7/HS24/reverse-indexing/task/script.py
@@ -23,14 +23,3 @@ def reverse_index(dataset):
 
 # You can see the output of your function here
 print(reverse_index(dataset))
-
-
-# Your task is to write an algorithm that takes a list of strings as input and returns a dictionary, containing an index of the words to the matching strings.
-
-# In the dictionary, each key will be a word k, while the value will be a list of indices of the input strings where the word k appears.
-
-# Words should be treated as lowercase only. i.e. Hello and hello should be treated the same.
-
-# You can assume that the dataset will contain only lists of strings. You don't need to check the type of the elements in the dataset.
-
-# The string data in the dataset will be clean. This means you don't need to worry about cleaning i.e. removing punctation marks or numbers.
