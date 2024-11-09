c99eeb2751493b8224c880f85425d3f03c8f61d3
RealBlueSwan <nobrunner@hotmail.com>
2024-11-09 14:22:44 +0100

    del reason: lack of understanding

--- access/a7/HS24/matrix-abstraction/task/tests.py
+++ access/a7/HS24/matrix-abstraction/task/tests.py
@@ -6,20 +6,37 @@ from task.script import Matrix
 
 class PublicTestSuite(TestCase):
 
-    # This test mixes a lot of different things.
-    # You should probably separate it into multiple tests and add some more
-    def test_basic_examples(self):
-        # instantiation
-        m = Matrix([[5,5],[5,5]])
-        t = Matrix([[5,5],[5,5]])
-        # equal?
+    def test_instantiation(self):
+        m = Matrix([[5, 5], [5, 5]])
+        self.assertIsInstance(m, Matrix)
+
+    def test_equality(self):
+        m = Matrix([[5, 5], [5, 5]])
+        t = Matrix([[5, 5], [5, 5]])
         self.assertTrue(m == t)
-        # hashes equal?
+
+    def test_hash_equality(self):
+        m = Matrix([[5, 5], [5, 5]])
+        t = Matrix([[5, 5], [5, 5]])
         self.assertTrue(hash(m) == hash(t))
-        # can be used as dictionary key?
+
+    def test_dictionary_key(self):
+        m = Matrix([[5, 5], [5, 5]])
+        t = Matrix([[5, 5], [5, 5]])
         d = {m: "1", t: "2"}
         d.update({m: "3"})
-        # add and multiply
-        m + t
-        m * t
+        self.assertEqual(d[m], "3")
+        self.assertEqual(d[t], "3")
+
+    def test_addition(self):
+        m = Matrix([[5, 5], [5, 5]])
+        t = Matrix([[5, 5], [5, 5]])
+        result = m + t
+        self.assertEqual(result, Matrix([[10, 10], [10, 10]]))
+
+    def test_multiplication(self):
+        m = Matrix([[5, 5], [5, 5]])
+        t = Matrix([[5, 5], [5, 5]])
+        result = m * t
+        self.assertEqual(result, Matrix([[50, 50], [50, 50]]))
 
--- access/a7/HS24/reverse-indexing/task/script.py
+++ access/a7/HS24/reverse-indexing/task/script.py
@@ -8,10 +8,29 @@ dataset = [
  ] 
 
 def reverse_index(dataset):
-    
     index_dictionary = {}
+    for idx in range(len(dataset)):
+        for word in dataset[idx].split():
+            word = word.lower()
+            # Check if word exists, if it does, append the value, else create a new list entry
+            if word in index_dictionary:
+                index_dictionary[word].append(idx)
 
-    # don't forget to return your resulting dictionary
+            else:
+                index_dictionary[word] = [idx]
+
+    return index_dictionary
 
 # You can see the output of your function here
 print(reverse_index(dataset))
+
+
+# Your task is to write an algorithm that takes a list of strings as input and returns a dictionary, containing an index of the words to the matching strings.
+
+# In the dictionary, each key will be a word k, while the value will be a list of indices of the input strings where the word k appears.
+
+# Words should be treated as lowercase only. i.e. Hello and hello should be treated the same.
+
+# You can assume that the dataset will contain only lists of strings. You don't need to check the type of the elements in the dataset.
+
+# The string data in the dataset will be clean. This means you don't need to worry about cleaning i.e. removing punctation marks or numbers.
