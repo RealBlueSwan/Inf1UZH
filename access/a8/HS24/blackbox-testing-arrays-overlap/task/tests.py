effe96917cf80f82d45f64d5901053a43bcd0ae5
RealBlueSwan <nobrunner@hotmail.com>
2024-11-12 19:06:46 +0100

    added new tasks

---
+++ access/a8/HS24/blackbox-testing-arrays-overlap/task/tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from unittest import TestCase
+from task.script import sorted_arrays_overlap
+
+# Implement this test suite. Make sure that you define test
+# methods and that each method _directly_ includes an assertion
+# in the body, or -otherwise- the grading will mark the test
+# suite as invalid.
+
+
+class SortedArraysOverlapTest(TestCase):
+    pass
