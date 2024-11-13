57e0bb59ff0396a2e8a31b6e958255f0a76a9d67
RealBlueSwan <nobrunner@hotmail.com>
2024-11-13 17:54:53 +0100

    Completed sorted_arrays_overlap a8 HS24

--- access/a8/HS24/blackbox-testing-arrays-overlap/task/script.py
+++ access/a8/HS24/blackbox-testing-arrays-overlap/task/script.py
@@ -6,17 +6,41 @@
 # This signature is required for the automated grading to work.
 # You must not rename the function or change its list of parameters.
 
-def sorted_arrays_overlap(array_1, array_2):
-	pass
+def sorted_arrays_overlap(array_1, array_2): 
+	# returns a list with the the elements that are in both lists
+	"""
+	* These arrays are non-empty lists of any integers sorted in ascending order.
+	* Arrays may have different length and may have duplicate elements.
+	"""
+	
+	if type(array_1) is not list or type(array_2) is not list:	# a string `Invalid input types! Both parameters should be arrays.` if either of the arguments is not of type `list`.
+		return "Invalid input types! Both parameters should be arrays."
+	
+	if not array_1 or not array_2:	# a string `Empty arrays! Neither of the arrays can be empty.` if either of the arguments is an empty list.
+		return "Empty arrays! Neither of the arrays can be empty."
+	
+	for k in array_2:
+		if type(k) is not int:
+			return "Invalid data types within arrays! Arrays should contain only integers."
+	for k in array_1:
+		if type(k) is not int:
+			return "Invalid data types within arrays! Arrays should contain only integers."
+		
+	if not (all(array_1[i] <= array_1[i + 1] for i in range(len(array_1) - 1))):
+		return "Not sorted arrays! Both arrays should be sorted in ascending order."
+	
+	if not (all(array_2[i] <= array_2[i + 1] for i in range(len(array_2) - 1))):
+		return "Not sorted arrays! Both arrays should be sorted in ascending order."
+	
 
 
-# Examples:
-# array_1 = [-5, -3, -1, 7, 8, 14, 19]
-# array_2 = [-8, -5, -2, -1, 19]
-# result = sorted_arrays_overlap(array_1, array_2)
-# print(result)  # [-5, -1, 19]
-#
-# array_1 = [5, 8, 14, 19, 19, 19, 23, 34, 52]
-# array_2 = [2, 5, 7, 8, 15, 19, 19, 25, 34, 62]
-# result = sorted_arrays_overlap(array_1, array_2)
-# print(result)  # [5, 8, 19, 19, 34]
+	result = []
+	for k in array_1:
+		if k in array_2:
+			array_2.remove(k)
+			result.append(k)
+
+	if result == []:
+		return "There are no overlapping elements in given arrays."
+	
+	return result
--- access/a8/HS24/blackbox-testing-arrays-overlap/task/tests.py
+++ access/a8/HS24/blackbox-testing-arrays-overlap/task/tests.py
@@ -9,4 +9,47 @@ from task.script import sorted_arrays_overlap
 
 
 class SortedArraysOverlapTest(TestCase):
-    pass
+    def test_example1(self):        # Checks for right output
+        array_1 = [-5, -3, -1, 7, 8, 14, 19]
+        array_2 = [-8, -5, -2, -1, 19]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), [-5, -1, 19])
+        
+    def test_example2(self):        # Check if its the correct output
+        array_1 = [5, 8, 14, 19, 19, 19, 23, 34, 52]
+        array_2 = [2, 5, 7, 8, 15, 19, 19, 25, 34, 62]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), [5, 8, 19, 19, 34])
+
+    def test_returns_list(self):    # Check if result is a list   
+        array_1 = [-5, -3, -1, 7, 8, 14, 19]
+        array_2 = [-8, -5, -2, -1, 19]
+        self.assertIsInstance(sorted_arrays_overlap(array_1, array_2), list)
+
+    def test_returns_empty_lists(self):
+        array_1 = [-5, -3, -1, 7, 8, 14, 19]
+        array_2 = [2, 3]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), "There are no overlapping elements in given arrays.")
+    
+    def test_check_for_empty_lists(self):   # a string `Empty arrays! Neither of the arrays can be empty.` if either of the arguments is an empty list.
+        array_1 = []
+        array_2 = []
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), "Empty arrays! Neither of the arrays can be empty.")
+
+    def test_not_lists(self):       # a string `Invalid input types! Both parameters should be arrays.` if either of the arguments is not of type `list`.
+        array_1 = {}
+        array_2 = (1, 2)
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), "Invalid input types! Both parameters should be arrays.")
+
+    def test_invalid_data_types_in_array(self):   # a string `Invalid data types within arrays! Arrays should contain only integers.` if either of the passed lists contains non-integer elements.
+        array_1 = [1, "hellothere"]
+        array_2 = [2, str]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), "Invalid data types within arrays! Arrays should contain only integers.")
+    
+    def test_ascending_order(self): #a string `Not sorted arrays! Both arrays should be sorted in ascending order.` if either of the passed lists is not sorted in ascending order.
+        array_1 = [2, 3, 4, 1]
+        array_2 = [1, 2, 3, 4]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), "Not sorted arrays! Both arrays should be sorted in ascending order.")
+
+    def test_identical_elements(self):  # Check if function handles arrays with identical elements correctly
+        array_1 = [1, 1, 1, 1]
+        array_2 = [1, 1, 1, 1]
+        self.assertEqual(sorted_arrays_overlap(array_1, array_2), [1, 1, 1, 1])
