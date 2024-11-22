c334672c2e6f81d6c6753b30781a33df69ff072e
RealBlueSwan <nobrunner@hotmail.com>
2024-11-22 15:12:48 +0100

    completed ispalindrom a9 HS24

--- access/a9/HS24/palidrome/task/script.py
+++ access/a9/HS24/palidrome/task/script.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-
+import re
 
 # Change the functions below to determine if the given input is a palindrome.
 
 def is_palindrome(s):
-    pass
+
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()  # Replace everything that isnt a-zA-Z0-9 to '' for cleanup this will enable the further processing
+
+    if len(s) == 0:         # Basecase is when the passed string is empty... potential BUG: when input is already [] then returns True
+        return True
+    
+    if s[0] != s[len(s)-1]: # if first and last char are not same is no palindrom
+        return False
+
+    return is_palindrome(s[1:len(s)-1])
 
 
 # The following lines call the function and prints the return
