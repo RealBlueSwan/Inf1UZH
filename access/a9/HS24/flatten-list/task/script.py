29446ea847c5fd9ccda7c4ffc966cf6d209be0ac
RealBlueSwan <nobrunner@hotmail.com>
2024-11-22 14:12:43 +0100

    completed flattenlist a9 HS24

--- access/a9/HS24/flatten-list/task/script.py
+++ access/a9/HS24/flatten-list/task/script.py
@@ -3,25 +3,24 @@
 # Change the functions below to flatten a given nested  list.
 def flatten_list(nested_list):
 
-    # if there is no list elemnnt in the list, return it 
-
-    if not any(isinstance(sub, list) for sub in nested_list):
-        return nested_list
-
-    if type(nested_list) != list:
+    if not isinstance(nested_list, list):
         raise TypeError("Expected argument has to be a list.")
+    # create a new list
+    new_list = []
 
-    list_without_nests = [element for element in nested_list if type(element) != list]
+    # Basecase when the nested list contains no sub lists.
+    for element in nested_list:
+        if isinstance(element, list):
+            new_list.extend(flatten_list(element))
+        else:
+            new_list.append(element)
 
+    return new_list 
     
-    return list_without_nests + nested_list[1]
-
-print(flatten_list([1, [2, 3]]))
-
 
 # The following lines call the function and print the return
 # value to the Console. This way you can check what they do.
-""""
+
 nested_list = [1, [2, [3, 4], 5], [6, 7], 8]
 flat_list = flatten_list(nested_list)
 print(flat_list)  # [1, 2, 3, 4, 5, 6, 7, 8]
@@ -30,4 +29,3 @@ print(flat_list)  # [1, 2, 3, 4, 5, 6, 7, 8]
 nested_list_2 = [[4.5, 6, "string"], "d", "g", ["f", 1, 6, ["another_string"]]]
 flat_list_2 = flatten_list(nested_list_2)
 print(flat_list_2)  # [4.5, 6, "string", "d", "g", "f", 1, 6, "another_string"]
-"""
