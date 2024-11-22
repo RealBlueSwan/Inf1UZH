9de81512d08b7cb93e1e1d249c848e1c925e36e6
RealBlueSwan <nobrunner@hotmail.com>
2024-11-22 16:01:14 +0100

    completed tower of hanoi a9 HS24

--- access/a9/HS24/towers_of_hanoi/task/script.py
+++ access/a9/HS24/towers_of_hanoi/task/script.py
@@ -4,12 +4,23 @@
 # Do not rename the function or change its list of parameters!
 def req_steps(num_disks):
     # implement this function
-    return -1
+
+    # Basecase is when there are no more steps to make and num_discs goes down to 0
+    if num_disks == 0:
+        return 0
+    
+    # Shift ‘N-1’ disks from ‘A’ to ‘B’, using C.
+    # Shift last disk from ‘A’ to ‘C’.
+    # Shift ‘N-1’ disks from ‘B’ to ‘C’, using A.
+
+    # Calls itself twice increases operations exponentially siehe pascals triangle
+
+    return 1 + req_steps(num_disks - 1) + req_steps(num_disks - 1)
 
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print("For moving {} disks, {} steps are required.".format(3, req_steps(3)))
+print("For moving {} disks, {} steps are required.".format(30, req_steps(30)))
 
