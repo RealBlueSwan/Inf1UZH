b0afca3220b54cfe5f8428d6ec19424c15ab6792
RealBlueSwan <nobrunner@hotmail.com>
2024-11-27 17:32:28 +0100

    done

--- access/a10/HS24/study-results/task/script.py
+++ access/a10/HS24/study-results/task/script.py
@@ -5,9 +5,31 @@ import os
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def get_average_grade(path):
-    if not os.path.exists(path):
-        pass # what to do?
-    return -1
+    if not os.path.exists(path):    # Return `None`, if the file does not exist 
+        return None   
+    
+    with open(path) as f:
+        content = f.read()
+
+        grades = []
+
+        for line in content.splitlines():           # split on newlines
+            if '#' in line:
+                continue
+            if not line:
+                continue
+
+            row = line.strip().replace(' ', '')     # split on :
+            row = row.split(':')
+            
+            course, grade = str(row[0]), float(row[1])
+
+            grades.append(grade)
+
+        if len(grades) == 0:                        # `0.0` if the file does not contain any grades.
+            return 0.0
+        
+        return sum(grades)/len(grades)
 
 
 # The following line calls the function and prints the return
