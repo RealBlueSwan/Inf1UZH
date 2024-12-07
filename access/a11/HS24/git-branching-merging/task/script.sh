8db455dd24c549ec1a0e28d09fa06eac1ea3e665
RealBlueSwan <nobrunner@hotmail.com>
2024-12-07 16:19:47 +0100

    done

--- access/a11/HS24/git-branching-merging/task/script.sh
+++ access/a11/HS24/git-branching-merging/task/script.sh
@@ -20,3 +20,39 @@ echo "${SOME_VAR}"
 # in the instructions:
 # REPO_URL="some-url" ./script.sh
 
+# Clone the repository at `$REPO_URL` into the folder `repo`.
+git clone "${REPO_URL}" repo
+
+# Change the working directory to the new folder (using the command `cd`).
+cd repo
+
+# Create a new branch with the name `feature_x`
+git branch feature_x
+
+# Switch to the newly created branch
+git checkout feature_x
+
+# Create a new file `hello.py` with the content `print("Hello World!")`
+echo 'print("Hello World!")' > hello.py
+
+# Add and commit the changes using the message `Add "Hello World" example`.
+git add hello.py
+git commit -m 'Add "Hello World" example'
+
+# Push the new branch to the repository (branches that do not yet exist on the remote must be explicitly named, for example, by invoking `git push origin «your branch»`)
+git push origin feature_x
+
+# Switch back to the `main` branch
+git checkout main
+
+# To emulate another developer that has worked on the repository in parallel, create a file `bye.py` with the content `# Good Bye`, add it to the index, and commit it to the `main` branch with the commit message `Add "Good Bye" comment`.
+echo '# Good Bye' > bye.py
+git add bye.py
+git commit -m 'Add "Good Bye" comment'
+
+# Now, merge your own feature branch into the `main` branch and use the commit message `Merge "feature_x"`. Like `commit`, `merge` would normally open an editor, but ACCESS is non-interactive, so use the `-m` parameter.
+git merge feature_x -m 'Merge "feature_x"'
+
+# Push `main` to the remote.
+git push origin main
+
